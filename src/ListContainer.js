c84c8b8460a3a960e773da55c65995f346905534
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-13 10:12:28 -0500

    separate list for checked items

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function ListContainer(props) {
-  // const unchecked = props.toDoList.filter(entry => !entry.check)
-  // const checked = props.toDoList.filter(entry => entry.check)
+  const unchecked = props.toDoList.filter(entry => !entry.check)
+  const checked = props.toDoList.filter(entry => entry.check)
 
-  const uncheckedItems = props.toDoList.map((entry, i) => {
+  const uncheckedItems = unchecked.map((entry, i) => {
     return (
       <div key={i} className="item">
         <input
@@ -20,21 +20,21 @@ export default function ListContainer(props) {
     )
   })
 
-  // const checkedItems = checked.map((entry, i) => {
-  //   return (
-  //     <div key={i} className="item">
-  //       <input
-  //         type="checkbox"
-  //         id={i}
-  //         tabIndex="0"
-  //         checked={entry.check}
-  //         className="checkbox"
-  //         onChange={props.handleCheck}
-  //       />
-  //       <label htmlFor={i}>{entry.toDoItem}</label>
-  //     </div>
-  //   )
-  // })
+  const checkedItems = checked.map((entry, i) => {
+    return (
+      <div key={i} className="item">
+        <input
+          type="checkbox"
+          id={entry.id}
+          tabIndex="0"
+          checked={entry.check}
+          className="checkbox"
+          onChange={props.handleCheck}
+        />
+        <label htmlFor={entry.id}>{entry.toDoItem}</label>
+      </div>
+    )
+  })
 
   return (
     <section className="listContainer">
@@ -53,7 +53,8 @@ export default function ListContainer(props) {
       </div>
       <hr />
       <div id="listItems">{uncheckedItems}</div>
-
+      <hr />
+      <div id="listItems">{checkedItems}</div>
       <p id="confirmation" className="confirmation" aria-live="assertive">
         {props.confirmation}
       </p>
