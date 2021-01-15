4fcea7806d8f5ab0152f1890dc47dcb2eb0b329a
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-15 10:39:36 -0500

    Added Github link; plus link title attributes

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
   return (
     <footer>
       <p>
         Based on Dave Gray's excellent{' '}
-        <a href="https://youtu.be/y51Cv4wnsPw" target="_blank" rel="noreferrer">
+        <a
+          href="https://youtu.be/y51Cv4wnsPw"
+          target="_blank"
+          rel="noreferrer"
+          title="link to tutorial on YouTube"
+        >
           video tutorial
         </a>
       </p>
@@ -15,8 +22,18 @@ export default function Footer() {
           href="https://steviegill-webportfolio.netlify.app/"
           target="_blank"
           rel="noreferrer"
+          title="Stevie's web app portofolio page"
         >
           Stevie Gill
+        </a>{' '}
+        <a
+          href="https://github.com/caffeinated-pixels/todolist-react"
+          target="_blank"
+          rel="noreferrer"
+          title="To-Do app Github repo"
+          className="githubIcon"
+        >
+          <FontAwesomeIcon icon={faGithubSquare} />
         </a>
       </p>
     </footer>
