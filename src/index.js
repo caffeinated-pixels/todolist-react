c957aa6cc308378865d1e5dc3ee07d86a09075a2
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-04-28 11:55:35 -0400

    Update index for react18

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './styles/main.scss'
 import App from './App'
 import 'typeface-roboto'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
