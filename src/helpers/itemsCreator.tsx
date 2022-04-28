0920b457f4e9b967c52f6fe1c90003f39e8cad34
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-04-28 15:08:11 -0400

    Convert ItemsCreator to tsx

--- src/helpers/itemsCreator.tsx
+++ src/helpers/itemsCreator.tsx
@@ -1,14 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
-export default function itemsCreator(props, isChecked) {
-  let filtered = []
+interface ToDoItem {
+  id: number
+  check: boolean
+  toDoItem: string
+}
 
+interface Props {
+  toDoList: ToDoItem[]
+  handleCheck: (stringId: string) => void
+  handleRemove: (stringId: string) => void
+  handleClearChecked?: () => void
+}
+
+export default function itemsCreator(props: Props, isChecked: boolean) {
+  let filtered = []
   // tests whether to filter for checked or unchecked items
   if (!isChecked) {
-    filtered = props.toDoList.filter(entry => !entry.check)
+    filtered = props.toDoList.filter((entry) => !entry.check)
   } else {
-    filtered = props.toDoList.filter(entry => entry.check)
+    filtered = props.toDoList.filter((entry) => entry.check)
   }
 
   return filtered.map((entry, i) => {
@@ -18,7 +30,7 @@ export default function itemsCreator(props, isChecked) {
           <input
             type="checkbox"
             id={entry.id}
-            tabIndex="0"
+            tabIndex={0}
             checked={entry.check}
             className="checkbox"
             onChange={() => props.handleCheck(entry.id)}
@@ -31,7 +43,7 @@ export default function itemsCreator(props, isChecked) {
           className="delete"
           onClick={() => props.handleRemove(entry.id)}
           role="button"
-          tabIndex="0"
+          tabIndex={0}
           aria-label="Delete item"
           aria-hidden="false"
           focusable="true"
