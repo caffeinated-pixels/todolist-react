283f2c4acb762a74582613781c7dde250b450373
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-09-17 09:09:11 -0400

    chore: cleanup comments

--- src/components/NewItemEntry.tsx
+++ src/components/NewItemEntry.tsx
@@ -41,32 +41,3 @@ const NewItemEntry = forwardRef<HTMLInputElement, Props>((props, ref) => {
 })
 
 export default NewItemEntry
-
-// export default function NewItemEntry(props: Props) {
-//   return (
-//     <section className="newItemEntry">
-//       <form id="itemEntryForm" onSubmit={props.handleSubmit}>
-//         <label htmlFor="newItemEntry">Enter a new to-do item</label>
-//         <input
-//           id="newItem"
-//           type="text"
-//           size={40}
-//           autoComplete="off"
-//           placeholder="Add Item"
-//           tabIndex={0}
-//           value={props.value}
-//           onChange={props.handleTextInput}
-//         />
-//         <button
-//           id="addItem"
-//           className="button"
-//           title="Add new item"
-//           aria-label="Add new item to list"
-//           tabIndex={0}
-//         >
-//           <FontAwesomeIcon icon={faPlus} />
-//         </button>
-//       </form>
-//     </section>
-//   )
-// }
